09feb11c84639e6f18e5f9044692526341b5db5d
b-mahadevan <mahadevanb2002@gmail.com>
2025-05-08 14:51:30 +0530

    Add customized line graph

---
+++ simple_visualizations/customized_line_graph/main.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+
+input_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+fibonacci_sequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+fig, ax = plt.subplots(figsize = (7, 5))
+
+ax.plot(fibonacci_sequence, linewidth = 3)
+
+ax.set_title('Fibonacci Sequence Plot', fontsize=25)
+ax.set_xlabel('Index (n)', fontsize=15)
+ax.set_ylabel('Fibonacci Number F(n)', fontsize=15)
+
+ax.tick_params(labelsize=14)
+
+plt.show()
