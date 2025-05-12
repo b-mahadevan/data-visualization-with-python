92035cfcd52894eb811b1c51368d9c36701a050d
b-mahadevan <mahadevanb2002@gmail.com>
2025-05-12 13:09:26 +0530

    Modify and update file under eq_data_1_day directory

---
+++ earthquake_data/eq_data_1_day/readable_eq_data.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import json
+
+#Convert json object into python object using json.loads()
+path = Path('D:\python\data_visualization_with_python\earthquake_data\eq_data_1_day_m1.geojson')
+content = path.read_text(encoding='utf-8')
+all_eq__data = json.loads(content)
+
+#Convert python object into json object using json.loads(), write and save it as a file
+path = Path(r'D:\python\data_visualization_with_python\earthquake_data\readable_eq_data.geojson')
+readable_content = json.dumps(all_eq__data, indent = 4)
+path.write_text(readable_content, encoding='utf-8')
